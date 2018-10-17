2d28321e2c788d8bbe0f019fda2d0f020ac4cd5a
maeganwilson_ <maegan@maeganwilson.com>
2018-10-17 10:04:31 -0500

    Finished connecting actions and objects

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -12,5 +12,13 @@ class ViewController: UIViewController {
 
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        
+    }
 
 }

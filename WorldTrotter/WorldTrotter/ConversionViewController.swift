f4d1a06877035dacb02d4c23fecaee6b50ca8e15
maeganwilson_ <maegan@maeganwilson.com>
2018-10-17 13:50:55 -0500

    Added converting ability

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -11,16 +11,43 @@ import UIKit
 class ConversionViewController: UIViewController {
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet{
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue:Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = "\(celsiusValue.value)"
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
     
     @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer){
         textField.resignFirstResponder()
     }
     
     @IBAction func fahrenheitFielEditingChanged(_ textField: UITextField){
-        if let text = textField.text, !text.isEmpty{
-            celsiusLabel.text = text
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
         } else {
-            celsiusLabel.text = textField.text
+            fahrenheitValue = nil
         }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateCelsiusLabel()
     }
 }

1a84db2bb06468f57867a96d7c9d2918a3c7f17c
maeganwilson_ <maegan@maeganwilson.com>
2018-10-17 16:49:34 -0500

    Created a view programmitcally

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -7,8 +7,17 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        mapView = MKMapView()
+        view = mapView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         

12ef48767dbe96e5028ef1a7194f41f0bdfc09c1
maeganwilson_ <maegan@maeganwilson.com>
2018-10-17 15:39:50 -0500

    Added a map and a tab controller to switch between views

---
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Maegan Wilson on 10/17/18.
+//  Copyright © 2018 MW. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view.")
+    }
+}
